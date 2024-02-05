5d40137b9058a49a2d292e6ae9b908f39183d826
pedrohambrosioo <pedro.ambro@hotmail.com>
2024-02-05 11:06:39 -0300

    FINALIZADO

--- setup/settings.py
+++ setup/settings.py
@@ -132,6 +132,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+DEBUG = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
